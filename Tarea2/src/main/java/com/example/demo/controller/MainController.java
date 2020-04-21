6f8c444609a130fd63f19fb24f692f29404850da
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2020-04-20 20:30:40 -0600

    Tarea 2 parte 2

--- Tarea2/src/main/java/com/example/demo/controller/MainController.java
+++ Tarea2/src/main/java/com/example/demo/controller/MainController.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Calendar;
 
 import org.springframework.stereotype.Controller;
@@ -16,5 +19,19 @@ public class MainController {
 		String info = "Mariana Molina | 00046146 | Ingenieria Informatica | 3° año" ;
 		return info;
 	}
+	
+	@RequestMapping("/dia")
+	public @ResponseBody String dia(HttpServletRequest dato1, HttpServletRequest dato2) {
+		Integer fecha = Integer.parseInt(dato1.getParameter("fecha"));
+		Integer annio = Integer.parseInt(dato2.getParameter("annio"));
+		
+		
+		LocalDate localDate = LocalDate.of(annio, Month.APRIL, fecha);
+
+		DayOfWeek DIA = DayOfWeek.from(localDate); 
+		
+		return DIA.name();
+	}
+	
 	
 }
